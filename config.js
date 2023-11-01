f9885bef58e021ddcc31244036cc6c539bbe4ce4
Joe Oak <41307427+joeoak@users.noreply.github.com>
2023-11-01 22:52:21 +0000

    Add another secret

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 const octocatToken = "octocat_token_abc123";
-const figmaToken = "figd_Xy7F4Dv1g2E9A8Bp3K6N5ZxRtLs7Qw2i4M0OjP9a";
+const figmaToken1 = "figd_Xy7F4Dv1g2E9A8Bp3K6N5ZxRtLs7Qw2i4M0OjP9a";
 const figmaToken2 = "figd_Xy7F4Dv1g2E9A8Bp3K6N5ZxRtLs7Qw2i4M0OjP9b";
+const figmaToken3 = "figd_Xy7F4Dv1g2E9A8Bp3K6N5ZxRtLs7Qw2i4M0OjP9c";
